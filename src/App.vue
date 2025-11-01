75240bf4d3e27b2a1d8589d5823e7a8d6d67b1ff
kdk1026 <daekwang1026@gmail.com>
2025-11-01 22:40:39 +0900

    업데이트

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
+import { useHead } from '@unhead/vue';
 import ErrorBoundary from './components/ErrorBoundary.vue';
 import MenuBar from './components/layouts/MenuBar.vue';
+import { useCustomBackNavigation } from './compositions/useCustomBackNavigation';
+
+const title = import.meta.env.VITE_APP_TITLE;
+
+useCustomBackNavigation();
+
+useHead({
+  title: title,
+  meta: [
+    { property: 'og:title', content: title }
+  ]
+});
 </script>
 
 <template>
   <ErrorBoundary>
+    <metainfo></metainfo>
     <MenuBar />
     <router-view :key="$route.fullPath"></router-view>
   </ErrorBoundary>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import { router } from './router/index';
 import VueCookies from 'vue-cookies';
 import store from './store';
+import { createHead } from '@unhead/vue/client'
 
 const app = createApp(App);
+
+const head = createHead();
+app.use(head);
+
 app.use(router);
 app.use(store);
 app.use(VueCookies);
+
 app.mount('#app');
